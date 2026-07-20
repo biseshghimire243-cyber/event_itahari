9ed9eba133b457f794c2b28bdc5b04db343e77f0
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-20 04:01:41 -0700

    the frontend is updated

---
+++ frontend/auth.js
@@ -0,0 +1,56 @@
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+
+    loginForm.addEventListener("submit", async (e) => {
+
+        e.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        try {
+
+            const response = await fetch("http://localhost:3000/login", {
+
+                method: "POST",
+
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+
+                localStorage.setItem("token", data.token);
+                localStorage.setItem("user", JSON.stringify(data.user));
+
+                alert("Login Successful!");
+
+                window.location.href = "events.html";
+
+            } else {
+
+                alert(data.message);
+
+            }
+
+        } catch (error) {
+
+            console.log(error);
+
+            alert("Server Error");
+
+        }
+
+    });
+
+}
