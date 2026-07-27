1ea53f5a3e066c07e12e0ea617583d60c22d2f7e
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-27 08:57:46 -0700

    the contav=ct seesssion are generatedd

--- frontend/js/contact.js
+++ frontend/js/contact.js
@@ -4,7 +4,23 @@ form.addEventListener("submit", function (e) {
 
     e.preventDefault();
 
-    alert("Thank you! Your message has been sent successfully.");
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const eventType = document.getElementById("event").value;
+    const message = document.getElementById("message").value;
+
+    if (
+        name === "" ||
+        email === "" ||
+        phone === "" ||
+        message === ""
+    ) {
+        alert("Please fill all required fields.");
+        return;
+    }
+
+    alert("🎉 Thank you! Your message has been sent successfully.");
 
     form.reset();
 
